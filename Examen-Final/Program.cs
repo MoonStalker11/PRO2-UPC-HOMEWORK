0d82cc9df788c32b23efd7011aef6532c0bf2cc1
MoonStalker11 <122495480+MoonStalker11@users.noreply.github.com>
2023-11-30 20:03:17 -0400

    V3

--- Examen-Final/Program.cs
+++ Examen-Final/Program.cs
@@ -2,9 +2,7 @@
     public class Program{
         public static void Main(string[] args)
         {
-            
-
-            
+            /*
             //Genra una serie iterativa
             Console.WriteLine("Examen Final");
 
@@ -23,6 +21,26 @@
             FicheroRonald fd = new FicheroRonald(path);
             fd.EscribirArray(AC1.ArrayCadenas);
             //Console.WriteLine();
+            */
+
+            for(int a = 1; a<=10;a++){
+            //Genra una serie iterativa
+            Console.WriteLine("Examen Final");
+
+            int cant = 10;
+            ClaseArrayCadenas AC1 = new ClaseArrayCadenas(cant);
+
+            // Llamada al método GenerarSerieExamenIterativo
+            AC1.GenerarSerieExamenIterativo(true, 5, 10, cant);
+        
+            // Mostrar el array después de generar la serie
+            AC1.MostrarArray();
+            
+            string path = "D:/UPC/Github/PRO2-UPC-HOMEWORK/Examen-Final/for/IterativoArchivo" + a + ".txt";
+            FicheroRonald fd = new FicheroRonald(path);
+            fd.EscribirArray(AC1.ArrayCadenas);
+                
+            }
             
 
         }
